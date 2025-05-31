d6d1619682debed54890d0c09e6a57307b5d9859
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-05-31 11:04:33 +0300

    Added Yandex Metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 import "./globals.css";
 import Header from "./components/header/Header";
 import { Providers } from "./providers";
@@ -43,23 +44,50 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <head>
-          <script
-            data-noptimize="1"
-            data-cfasync="false"
-            data-wpfc-render="false"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function() {
-                  var script = document.createElement("script");
-                  script.async = 1;
-                  script.src = 'https://emrld.cc/NDIxMjAw.js?t=421200';
-                  document.head.appendChild(script);
-                })();
-              `
-            }}
-          />
-        </head>
+        <Script
+          id="emrld-script"
+          data-noptimize="1"
+          data-cfasync="false"
+          data-wpfc-render="false"
+          strategy="afterInteractive"
+        >
+          {`
+            (function() {
+              var script = document.createElement("script");
+              script.async = 1;
+              script.src = 'https://emrld.cc/NDIxMjAw.js?t=421200';
+              document.head.appendChild(script);
+            })();
+          `}
+        </Script>
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(102298581, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/102298581" 
+              style={{ position: 'absolute', left: '-9999px' }} 
+              alt="" 
+            />
+          </div>
+        </noscript>
         <Providers>
           <Header />
           {children}
